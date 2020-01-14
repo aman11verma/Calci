adf7b7845062f67c0b4c1bb699402e364575de36
aman11verma <amanverma.work@gmail.com>
2020-01-15 03:20:41 +0530

    Created winow and added test button

--- calci.py
+++ calci.py
@@ -1,19 +1,33 @@
 # coding: utf-8
 import sys
-from PyQt5 import QtGui
+from PyQt5 import QtGui,QtCore
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QPushButton
 
-def window():
-   app = QApplication(sys.argv)
-   w = QWidget()
-   b = QLabel(w)
-   b.setText("Hello World!")
-   w.setGeometry(100,100,200,50)
-   b.move(50,20)
-   w.show()
-   sys.exit(app.exec_())
+def calci_dimensions(window):
+   
+   window.setWindowTitle('Calculator')
+   window.setGeometry(500,300,600,600)
+   window.move(50,20)
+   
+
+def create_buttons(window):
+      btn = QPushButton("Quit",window)
+      btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+      btn.resize(100,100)
+      btn.move(100,100)
+      window.show()
+   
 	
+def main():
+   calci = QApplication(sys.argv)
+   window = QWidget()
+   calci_dimensions(window)
+   create_buttons(window)
+   window.show()
+   sys.exit(calci.exec_())
+
 if __name__ == '__main__':
-   window()
+   main()
